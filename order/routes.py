4d37722d9ee875e70eac353c89541548ae42dbb6
docscodes <docscodes@gmail.com>
2025-07-31 19:49:57 -0400

    call user api

--- order/routes.py
+++ order/routes.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, jsonify, request
+from models import Order, OrderItem, db
 import requests
-
 
 order_blueprint = Blueprint('order_api_routes', __name__, url_prefix="/api/order")
 
 USER_API_URL = 'http://127.0.0.1:5001/api/user'
+
+
+def get_user(api_key):
+    headers = {
+        'Authorization': api_key
+    }
+
+    response = requests.get(USER_API_URL, headers=headers)
+    if response.status_code != 200:
+        return {'message': 'Not Authorized'}
+
+    user = response.json()
+    return user
 
 
 @order_blueprint.route('/', methods=['GET'])
@@ -14,7 +27,9 @@ def get_open_order():
 
 @order_blueprint.route('/all', methods=['GET'])
 def all_orders():
-   return "All orders"
+    orders = Order.query.all()
+    result = [order.serialize() for order in orders]
+    return jsonify(result), 200
 
 
 @order_blueprint.route('/add-item', methods=['POST'])
