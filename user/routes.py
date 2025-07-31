c2c0bf714f80861dd7f58a147bb3075def3ab747
docscodes <docscodes@gmail.com>
2025-07-31 10:37:44 -0400

    create user

--- user/routes.py
+++ user/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
-from models import User
+from flask import Blueprint, jsonify, request
+from models import User, db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 user_blueprint = Blueprint('user_api_routes', __name__, url_prefix='/api/user')
@@ -15,4 +16,23 @@ def get_all_users():
         'result': result
     }
     
+    return jsonify(response)
+
+
+@user_blueprint.route('/create', methods=['POST'])
+def create_user():
+    try:
+        user = User()
+        user.username = request.form["username"]
+        user.password = generate_password_hash(request.form['password'], method='scrypt')
+
+        user.is_admin = False
+
+        db.session.add(user)
+        db.session.commit()
+
+        response = {'message': 'User Created', 'result': user.serialize()}
+    except Exception as e:
+        print(str(e))
+        response = {'message': 'Error in creating response'}
     return jsonify(response)
