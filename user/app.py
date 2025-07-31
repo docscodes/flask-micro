ab2de5e1c283d68462e71c72d3e2f90bf09e0f55
docscodes <docscodes@gmail.com>
2025-07-31 09:07:10 -0400

    get all users

--- user/app.py
+++ user/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from routes import user_blueprint
-from models import models
+import models
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 
+database_path = os.path.join(os.path.dirname(__file__), 'database/user.db')
 
 app.config['SECRET_KEY'] = 'vZK7siKrV8BL46KsyAIPoQ'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
 models.init_app(app)
 
 
 app.register_blueprint(user_blueprint)
 
+migrate = Migrate(app, models.db)
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
--- user/routes.py
+++ user/routes.py
@@ -1,8 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from models import User
+
 
 user_blueprint = Blueprint('user_api_routes', __name__, url_prefix='/api/user')
 
 
-@user_blueprint.route('/')
-def index():
-    return 'Hello World!'
+@user_blueprint.route('/all', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    result = [user.serialize() for user in users]
+    
+    response = {
+        'message': 'success',
+        'result': result
+    }
+    
+    return jsonify(response)
