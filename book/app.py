54eebe35c4176505bc32fbb5ae80642f260a3ebf
docscodes <docscodes@gmail.com>
2025-07-31 16:48:54 -0400

    init book db

--- book/app.py
+++ book/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
 from routes import book_blueprint
+from models import db, init_app
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
@@ -12,6 +15,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 app.register_blueprint(book_blueprint)
+init_app(app)
+migrate = Migrate(app, db)
 
 
 if __name__ == '__main__':
