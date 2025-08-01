285b6531cc4f2833821c34b94bcb2e74fa20b706
docscodes <docscodes@gmail.com>
2025-08-01 08:27:16 -0400

    user container name for service urls

--- frontend/api/__init__.py
+++ frontend/api/__init__.py
@@ -1,3 +1,7 @@
-USER_API_URL = 'http://127.0.0.1:5001'
-BOOK_API_URL = 'http://127.0.0.1:5002'
-ORDER_API_URL = 'http://127.0.0.1:5003'
+USER_API_URL = 'http://user-service-container:5001'
+BOOK_API_URL = 'http://book-service-container:5002'
+ORDER_API_URL = 'http://order-service-container:5003'
+
+# USER_API_URL = 'http://127.0.0.1:5001'
+# BOOK_API_URL = 'http://127.0.0.1:5002'
+# ORDER_API_URL = 'http://127.0.0.1:5003'
