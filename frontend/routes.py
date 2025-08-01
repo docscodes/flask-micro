862fad5d19aa2502ddd88f45d931478b3b9b49d6
docscodes <docscodes@gmail.com>
2025-08-01 07:57:08 -0400

    register

--- frontend/routes.py
+++ frontend/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from api.book_client import BookClient
 from api.user_client import UserClient
 from api.order_client import OrderClient
-
+import forms
 
 blueprint = Blueprint('frontend', __name__)
 
@@ -21,3 +21,22 @@ def index():
     return render_template('index.html', books=books)
 
 
+@blueprint.route('/register', methods=['POST', 'GET'])
+def register():
+    form = forms.RegistrationForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username = form.username.data
+
+            if UserClient.user_exists(username):
+                flash("Please try another user name")
+                return render_template('register.html', form=form)
+            else:
+                user = UserClient.create_user(form)
+                if user:
+                    flash("Registered. Please login.")
+                    return redirect(url_for('frontend.index'))
+        else:
+            flash("Errors")
+
+    return render_template('register.html', form=form)
