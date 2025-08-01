9748eb8e944e1f5b4a9286164c9cc51fe9fa3741
docscodes <docscodes@gmail.com>
2025-07-31 20:01:05 -0400

    create order

--- order/routes.py
+++ order/routes.py
@@ -34,7 +34,44 @@ def all_orders():
 
 @order_blueprint.route('/add-item', methods=['POST'])
 def add_order_item():
-    return "Add order item"
+    api_key = request.headers.get('Authorization')
+    if not api_key:
+        return jsonify({'message': 'Not logged in'}), 401
+    
+    response = get_user(api_key)
+    if not response.get('result'):
+        return jsonify({'message': 'Not logged in'}), 401
+
+    user = response.get('result')
+
+    book_id = int(request.form['book_id'])
+    quantity = int(request.form['quantity'])
+    user_id = user['id']
+
+    open_order = Order.query.filter_by(user_id=user_id, is_open=1).first()
+
+    if not open_order:
+        open_order = Order()
+        open_order.is_open = True
+        open_order.user_id = user_id
+
+        order_item = OrderItem(book_id=book_id, quantity=quantity)
+        open_order.order_items.append(order_item)
+    else:
+        found = False
+        for item in open_order.order_items:
+            if item.book_id == book_id:
+                item.quantity += quantity
+                found = True
+
+        if not found:
+            order_item = OrderItem(book_id=book_id, quantity=quantity)
+            open_order.order_items.append(order_item)
+
+    db.session.add(open_order)
+    db.session.commit()
+
+    return jsonify({"result": open_order.serialize()})
 
 
 @order_blueprint.route('/checkout', methods=['POST'])
