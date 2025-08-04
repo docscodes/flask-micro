32744b1a5f35b206ce3afe1eba226c993d3d0d9d
docscodes <docscodes@gmail.com>
2025-08-04 07:55:30 -0400

    add swagger for example

--- example/app.py
+++ example/app.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app, template={
+    "info": {
+        "title": "My Flask API",
+        "description": "An example API using Flask and Swagger",
+        "version": "1.0.0"
+    }
+})
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    """
+    This is an example endpoint that returns 'Hello, World!'
+    ---
+    tags:
+        - Greetings
+    description: Returns a friendly greeting.
+    responses:
+        200:
+            description: A successful response
+            examples:
+                application/json: "Hello, World!"
+    """
+    return {'message': 'Hello, World!'}
 
 
 if __name__ == '__main__':
